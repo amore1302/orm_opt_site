443b80221e68e98b652d9affd44a7d7d45327bb2
amore1302 <amore1302@mail.ru>
2020-12-27 19:44:44 +0300

    Optimize comments count evaluation

--- blog/views.py
+++ blog/views.py
@@ -47,17 +47,23 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = Post.objects.prefetch_related('author').annotate(num_likes=Count('likes',
-            distinct=True), num_coment=Count('comments', distinct=True)).order_by('-num_likes')[:5]
 
-    fresh_posts = Post.objects.prefetch_related('author').order_by('-published_at').annotate(num_coment=Count('comments'))[:5]
+    most_popular_posts = Post.objects.prefetch_related('author').annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
+    most_popular_posts_ids = [post.id for post in most_popular_posts]
+    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
+    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+    count_for_id = dict(ids_and_comments)
+    for post in most_popular_posts:
+        post.comments_count = count_for_id[post.id]
+
+    fresh_posts = Post.objects.prefetch_related('author').order_by('-published_at').annotate(comments_count=Count('comments'))[:5]
     most_fresh_posts = list(fresh_posts)
 
-    most_popular_tags = Tag.objects.annotate(num_tag=Count('posts') ).order_by('-num_tag')[:5]
+    most_popular_tags = Tag.objects.annotate(tags_count=Count('posts') ).order_by('-tags_count')[:5]
 
     context = {
-        'most_popular_posts': [serialize_post_optimized(post, post.num_coment) for post in most_popular_posts],
-        'page_posts': [serialize_post_optimized(post, post.num_coment) for post in most_fresh_posts],
+        'most_popular_posts': [serialize_post_optimized(post, post.comments_count) for post in most_popular_posts],
+        'page_posts': [serialize_post_optimized(post, post.comments_count) for post in most_fresh_posts],
         'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
     }
 
