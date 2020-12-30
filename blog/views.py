08444f57d8b1bba62f0f9c25bfae8ce7acfce9fc
amore1302 <amore1302@mail.ru>
2020-12-30 19:33:15 +0300

    Move queries into QuerySet methods

--- blog/views.py
+++ blog/views.py
@@ -46,14 +46,7 @@ def serialize_tag(tag):
 
 
 def index(request):
-
-    most_popular_posts = Post.objects.prefetch_related('author').annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
-    most_popular_posts_ids = [post.id for post in most_popular_posts]
-    posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(comments_count=Count('comments'))
-    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
-    count_for_id = dict(ids_and_comments)
-    for post in most_popular_posts:
-        post.comments_count = count_for_id[post.id]
+    most_popular_posts = Post.objects.popular().prefetch_related('author')[:5].fetch_with_comments_count()
 
     fresh_posts = Post.objects.prefetch_related('author').order_by('-published_at').annotate(comments_count=Count('comments'))[:5]
     most_fresh_posts = list(fresh_posts)
